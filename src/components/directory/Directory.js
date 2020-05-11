e23a052b764aca22033df03b530f513839e3c32a
karol-nogal <karolnogal92@gmail.com>
2020-05-11 21:48:40 +0200

    withRouter added

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -45,8 +45,8 @@ class Directory extends React.Component{
     render(){
         return(
             <div className="directory-menu">
-                {this.state.section.map(({title, imageUrl, id,size})=>{
-                   return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                {this.state.section.map(({title, imageUrl, id, size, linkUrl })=>{
+                   return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl}/>
                 })}
             </div>
         )

654882557a8614c114034687743503e65baba4d9
karol-nogal <karolnogal92@gmail.com>
2020-05-26 00:54:05 +0200

    userRef function added , allowing to unsubrscribe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -10,6 +10,7 @@ import SignInAndSignUpPage from './pages/sign-in-up/sign-in-up';
 import Header from './components/header/Header';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.acions';
+import SignInandSingUp from './pages/sign-in-up/sign-in-up';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -44,18 +45,22 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+    <Route path='/signin' render={()=>this.props.currentUser?(<Redirect to="/"/>):<SignInandSingUp/>} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user})=>({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);

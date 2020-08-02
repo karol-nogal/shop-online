7f8193eaacdb345b54e2af4b7289f99a3b2a4686
karol-nogal <karolnogal92@gmail.com>
2020-08-02 10:23:50 +0200

    new machine run , rewriting , reupdating dependencies

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,55 +1,50 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-  import firebase from "firebase/app"
-  import 'firebase/firestore'
-  import 'firebase/auth'
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDZ--eymte-pq3f1J2xu3yLsWcw2mCRPbE",
-    authDomain: "shop-online-f55f7.firebaseapp.com",
-    databaseURL: "https://shop-online-f55f7.firebaseio.com",
-    projectId: "shop-online-f55f7",
-    storageBucket: "shop-online-f55f7.appspot.com",
-    messagingSenderId: "605439584565",
-    appId: "1:605439584565:web:7cbfd9bbdcf27f581f847c",
-    measurementId: "G-PYYFW3E4TL"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+const config = {
+  apiKey: "AIzaSyDZ--eymte-pq3f1J2xu3yLsWcw2mCRPbE",
+  authDomain: "shop-online-f55f7.firebaseapp.com",
+  databaseURL: "https://shop-online-f55f7.firebaseio.com",
+  projectId: "shop-online-f55f7",
+  storageBucket: "shop-online-f55f7.appspot.com",
+  messagingSenderId: "605439584565",
+  appId: "1:605439584565:web:7cbfd9bbdcf27f581f847c",
+  measurementId: "G-PYYFW3E4TL"
+};
 
-  export const createUserProfileDocument = async (userAuth, additionalData)=>{
-      if(!userAuth) return;
+firebase.initializeApp(config);
 
-      const userRef = firestore.doc(`users/${userAuth.uid}`)
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
 
-      const spanShot =  await userRef.get()
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-      
+  const snapShot = await userRef.get();
 
-      if(!spanShot.exists){
-          const {displayName, email} = userAuth;
-          const createAt = new Date();
-
-          try{
-            await userRef.set({
-                displayName,
-                email,
-                createAt,
-                ...additionalData
-            })
-          }catch(error){
-              console.log('error ceating user',error.message)
-
-          }
-      }
-      return userRef;
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
   }
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({prompt:'select_account'});
+  return userRef;
+};
 
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  export default firebase;
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export default firebase;
